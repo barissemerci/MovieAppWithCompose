eb15f152252b662963240cc5109ee71e05fd6aaf
Barış SEMERCİ <barissemerci3@gmail.com>
2023-09-01 23:56:00 +0300

    add AppModule.kt

---
+++ app/src/main/java/com/barissemerci/movieappwithcompose/data/dependencyinjection/AppModule.kt
@@ -0,0 +1,33 @@
+package com.barissemerci.movieappwithcompose.data.dependencyinjection
+
+import com.barissemerci.movieappwithcompose.data.remote.MovieAPI
+import com.barissemerci.movieappwithcompose.data.repository.MovieRepositoryImpl
+import com.barissemerci.movieappwithcompose.domain.repository.MovieRepository
+import com.barissemerci.movieappwithcompose.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMovieApi(): MovieAPI {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build().create(MovieAPI::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(api: MovieAPI): MovieRepository {
+        return MovieRepositoryImpl(api)
+    }
+
+
+}
